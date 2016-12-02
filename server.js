2ae6d37e32ecd3cf6c44a5e179e3f511749e6cac
James Quiambao <james.quiambao@yahoo.ca>
2016-12-02 20:15:38 +0000

    Updated comments

--- server.js
+++ server.js
@@ -1,3 +1,23 @@
 'use strict'; 
 var express = require("express"); 
 var app = express(); 
+var port = process.env.PORT || 8080; 
+
+//Two routes
+
+//Image search 
+//Takes 2 parameters
+//Whatever you're searching for 
+    // I can get the image URLs, alt text and page urls for 
+    // a set of images relating to a given search string.
+app.get('/api/imagesearch/:search', function(req, res){
+    console.log(req.params.search + " " + req.query.offset); 
+});   
+
+    
+//pagination 
+//
+//Past image searches
+app.listen(port, function(){
+    console.log("App is listening on port:" + port); 
+})
